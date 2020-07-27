99bac15a2a19c9430331163d964136c8f8d4f89e
TauriKalve <tauri@digiway.ee>
2020-07-27 20:28:30 +0300

    fix?

--- api/article/models/article.js
+++ api/article/models/article.js
@@ -1,55 +1,86 @@
 'use strict';
-
+const axios = require('axios');
 /**
  * Lifecycle callbacks for the `article` model.
  */
 
+const test = () => {
+  axios.post('http://localhost:8000/__refresh');
+  console.log('---- GATSBY REBUILD ----');
+};
+
 module.exports = {
-  // Before saving a value.
-  // Fired before an `insert` or `update` query.
-  // beforeSave: async (model, attrs, options) => {},
+  lifecycles: {
+    // Before saving a value.
+    // Fired before an `insert` or `update` query.
+    beforeSave: async (model) => {
+      console.log('beforeSave: ');
+    },
 
-  // After saving a value.
-  // Fired after an `insert` or `update` query.
-  // afterSave: async (model, response, options) => {},
+    // After saving a value.
+    // Fired after an `insert` or `update` query.
+    afterSave: async (model, result) => {
+      console.log('afterSave: ');
+    },
 
-  // Before fetching a value.
-  // Fired before a `fetch` operation.
-  // beforeFetch: async (model, columns, options) => {},
+    // Before fetching all values.
+    // Fired before a `fetchAll` operation.
+    beforeFetchAll: async (model) => {
+      console.log('beforeFetchAll: ');
+    },
 
-  // After fetching a value.
-  // Fired after a `fetch` operation.
-  // afterFetch: async (model, response, options) => {},
+    // After fetching all values.
+    // Fired after a `fetchAll` operation.
+    afterFetchAll: async (model, results) => {
+      console.log('afterFetchAll: ');
+    },
 
-  // Before fetching all values.
-  // Fired before a `fetchAll` operation.
-  // beforeFetchAll: async (model, columns, options) => {},
+    // Fired before a `fetch` operation.
+    beforeFetch: async (model) => {
+      console.log('beforeFetch: ');
+    },
 
-  // After fetching all values.
-  // Fired after a `fetchAll` operation.
-  // afterFetchAll: async (model, response, options) => {},
+    // After fetching a value.
+    // Fired after a `fetch` operation.
+    afterFetch: async (model, result) => {
+      console.log('afterFetch: ');
+    },
 
-  // Before creating a value.
-  // Fired before an `insert` query.
-  // beforeCreate: async (model, attrs, options) => {},
+    // Before creating a value.
+    // Fired before an `insert` query.
+    beforeCreate: async (model) => {
+      console.log('beforeCreate: ');
+    },
 
-  // After creating a value.
-  // Fired after an `insert` query.
-  // afterCreate: async (model, attrs, options) => {},
+    // After creating a value.
+    // Fired after an `insert` query.
+    afterCreate: async (model, result) => {
+      console.log('afterCreate: ');
+    },
 
-  // Before updating a value.
-  // Fired before an `update` query.
-  // beforeUpdate: async (model, attrs, options) => {},
+    // Before updating a value.
+    // Fired before an `update` query.
+    beforeUpdate: async (model) => {
+      console.log('beforeUpdate: ');
+    },
 
-  // After updating a value.
-  // Fired after an `update` query.
-  // afterUpdate: async (model, attrs, options) => {},
+    // After updating a value.
+    // Fired after an `update` query.
+    afterUpdate: async (model, result) => {
+      console.log('afterUpdate: ');
+      test();
+    },
 
-  // Before destroying a value.
-  // Fired before a `delete` query.
-  // beforeDestroy: async (model, attrs, options) => {},
+    // Before destroying a value.
+    // Fired before a `delete` query.
+    beforeDestroy: async (model) => {
+      console.log('beforeDestroy: ');
+    },
 
-  // After destroying a value.
-  // Fired after a `delete` query.
-  // afterDestroy: async (model, attrs, options) => {}
+    // After destroying a value.
+    // Fired after a `delete` query.
+    afterDestroy: async (model, result) => {
+      console.log('afterDestroy: ');
+    }
+  }
 };
